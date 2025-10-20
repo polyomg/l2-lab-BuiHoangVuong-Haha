521accbc1db20fe432a703be448b216b662ee5ad
HoangVuongBui <buihoangvuong777@gmail.com>
2025-10-20 21:38:30 +0700

    PS43931-BuiHoangVuong-Lab6

---
+++ Lab6/src/main/java/com/poly/lab6/Controller/ProductController.java
@@ -0,0 +1,33 @@
+package com.poly.lab6.Controller;
+
+import com.poly.lab6.DAO.ProductDAO;
+import com.poly.lab6.Entity.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@Controller
+public class ProductController {
+    @Autowired
+    ProductDAO dao;
+
+    @RequestMapping("/product/sort")
+    public String sort(Model model, @RequestParam("field") Optional<String> field) {
+        Sort sort = Sort.by(Sort.Direction.DESC, field.orElse("price"));
+        model.addAttribute("field", field.orElse("price").toUpperCase());
+        model.addAttribute("items", dao.findAll(sort));
+        return "product/sort";
+    }
+
+    @RequestMapping("/product/page")
+    public String paginate(Model model, @RequestParam("p") Optional<Integer> p) {
+        Pageable pageable = PageRequest.of(p.orElse(0), 5);
+        Page<Product> page = dao.findAll(pageable);
+        model.addAttribute("page", page);
+        return "product/page";
+    }
+}
