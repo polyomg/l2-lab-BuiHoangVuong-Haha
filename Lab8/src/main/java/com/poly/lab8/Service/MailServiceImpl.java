697be4f2a49ec24ccee37e2d9c2c1caa2e5365fd
HoangVuongBui <buihoangvuong777@gmail.com>
2025-10-20 21:41:22 +0700

    PS43931-BuiHoangVuong-Lab8

---
+++ Lab8/src/main/java/com/poly/lab8/Service/MailServiceImpl.java
@@ -0,0 +1,69 @@
+package com.poly.lab8.Service;
+
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service("mailService")
+@EnableScheduling
+public class MailServiceImpl implements MailService {
+    @Autowired
+    JavaMailSender mailSender;
+
+    List<Mail> queue = new ArrayList<>();
+
+    @Override
+    public void send(Mail mail) {
+        try {
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true, "utf-8");
+
+            helper.setFrom(mail.getFrom());
+            helper.setReplyTo(mail.getFrom());
+            helper.setTo(mail.getTo());
+            if (!isNullOrEmpty(mail.getCc())) helper.setCc(mail.getCc());
+            if (!isNullOrEmpty(mail.getBcc())) helper.setBcc(mail.getBcc());
+            helper.setSubject(mail.getSubject());
+            helper.setText(mail.getBody(), true);
+
+            if (!isNullOrEmpty(mail.getFilenames())) {
+                for (String filename : mail.getFilenames().split("[,;]+")) {
+                    File file = new File(filename.trim());
+                    helper.addAttachment(file.getName(), file);
+                }
+            }
+
+            mailSender.send(message);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private boolean isNullOrEmpty(String text) {
+        return text == null || text.trim().isEmpty();
+    }
+
+    @Override
+    public void push(Mail mail) {
+        queue.add(mail);
+    }
+
+    @Scheduled(fixedDelay = 500)
+    public void run() {
+        while (!queue.isEmpty()) {
+            try {
+                this.send(queue.remove(0));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
